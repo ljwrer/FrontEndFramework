adda749001b1e7cd5778944dadedc1a249b2a6db
ljwrer <ljwrer@sina.com>
2015-07-20 17:27:46 +0800

    module

---
+++ js/calculator.js
@@ -0,0 +1,71 @@
+/* 反模式 */
+//var action="add";
+//function compute(a,b){
+//	switch (action){
+//		case "add":
+//			return add(a,b);
+//				break;
+//		case "sub":
+//			return sub(a,b);
+//				break;
+//		default:
+//			break;
+//	}
+//}
+
+/* 字面量 */
+//var calculator={
+//	action:"add",
+//	compute:function(a,b){
+//		switch(this.action){
+//			case "add":
+//				return math.add(a,b);
+//				break;
+//			case "sub":
+//				return math.sub(a,b);
+//				break;
+//			default:
+//				break;
+//		}
+//	}
+//}
+
+/* IIFE */
+//var calculator=(function(){
+//	var action="add";
+//	return {
+//		compute:function(a,b){
+//			switch(action){
+//				case "add":
+//					return math.add(a,b);
+//					break;
+//				case "sub":
+//					return math.sub(a,b);
+//					break;
+//				default:
+//					break;
+//			}
+//		}
+//	}
+//})();
+
+/* IIFE-review module pattern */
+//var calculator=(function(m){
+//	var action="add";
+//	var compute=function(a,b){
+//		switch(action){
+//			case "add":
+//				return m.add(a,b);
+//				break;
+//			case "sub":
+//				return m.sub(a,b);
+//				break;
+//			default:
+//				break;
+//		}
+//	}
+//	return {
+//		compute:compute
+//	};
+//})(math);
+
---
+++ js/math.js
@@ -0,0 +1,19 @@
+/* 反模式 */
+//function add(a,b){
+//	return a+b;
+//}
+//function sub(a,b){
+//	return a-b;
+//}
+
+/* 字面量 */
+var math={
+	add:function(a,b){
+		return a+b;
+	},
+	sub:function(a,b){
+		return a-b;
+	}
+}
+
+
